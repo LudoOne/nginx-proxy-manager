885bd1595d65d4dd02077fb940a826be2a068de8
Jamie Curnow <jamiec@benon.com>
2020-01-30 16:02:21 +1000

    up

--- backend/routes/api/schema.js
+++ backend/routes/api/schema.js
@@ -18,7 +18,18 @@ router
      * GET /schema
      */
     .get((req, res, next) => {
+        let proto = req.protocol;
+        if (typeof req.headers['x-forwarded-proto'] !== 'undefined' && req.headers['x-forwarded-proto']) {
+            proto = req.headers['x-forwarded-proto'];
+        }
+
+        let origin = proto + '://' + req.host;
+        if (typeof req.headers.origin !== 'undefined' && req.headers.origin) {
+            origin = req.headers.origin;
+        }
+
         swaggerJSON.info.version = PACKAGE.version;
+        swaggerJSON.servers[0].url = origin + '/api';
         res.status(200).send(swaggerJSON);
     });
 
